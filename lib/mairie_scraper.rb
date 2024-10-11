0546bfdb6a9bdb77d41162366694c97a81238f66
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-11 09:55:08 +0200

    modif

--- lib/mairie_scraper.rb
+++ lib/mairie_scraper.rb
@@ -13,18 +13,19 @@ def get_townhall_urls
   base_url = 'http://annuaire-des-mairies.com/val-d-oise'
   response = HTTParty.get(base_url)
   parsed_page = Nokogiri::HTML(response.body)
-  urls = parsed_page.css('a[href^="./"]').map { |link| " http://annuaire-des-mairies.com/#{link['href']}" }
+  urls = parsed_page.css('a[href^="./"]').map { |link| "http://annuaire-des-mairies.com/#{link['href'][1..]}" } # On enlève le premier caractère '.'
   urls
 end
 
 def fetch_all_townhall_emails
-  emails = []
+  emails = [] # Initialisation du tableau pour stocker les hashes
   get_townhall_urls.each do |url|
-    email = get_townhall_email(url) #recup email
-    emails << { url.split('/').last => email }  
+    email = get_townhall_email(url) # Récupérer l'email
+    townhall_name = url.split('/').last # Récupérer le nom de la mairie à partir de l'URL
+    emails << { townhall_name => email }  # Ajouter un hash au tableau
   end
   emails
 end
 
-#test récup emails
+# Test récup emails
 puts fetch_all_townhall_emails
